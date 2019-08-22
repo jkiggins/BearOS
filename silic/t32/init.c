762b598fade8d4f92bde978d0c4156d554e0c734
Jacob Kiggins <jkiggins794@gmail.com>
2019-08-21 22:50:55 -0400

    inital commit, architecture is WIP

---
+++ silic/t32/init.c
@@ -0,0 +1,15 @@
+/**
+ * @file init.c
+ * @author qzpv
+ * @brief 
+ * @version 0.1
+ * @date 2019-08-21
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+#include "init.h"
+#include "mk20d7.h"
+#include "../arm_cm4.h"
+
---
+++ silic/t32/init.h
@@ -0,0 +1,12 @@
+/**
+ * @file init.h
+ * @author qzpv
+ * @brief 
+ * @version 0.1
+ * @date 2019-08-21
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+void init();
